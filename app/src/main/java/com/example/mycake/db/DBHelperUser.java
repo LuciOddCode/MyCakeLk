6521e4b9130b1be8376e3d68945a856ddd2aa18f
LuciOddCode <dasanayakarusiru@gmail.com>
2024-07-14 17:41:52 +0530

    Changes has made

--- app/src/main/java/com/example/mycake/db/DBHelperUser.java
+++ app/src/main/java/com/example/mycake/db/DBHelperUser.java
@@ -17,6 +17,21 @@ public class DBHelperUser extends SQLiteOpenHelper {
     private static final String COLUMN_USER_PASSWORD = "user_password";
     private static final String COLUMN_USER_ROLE = "user_role";
 
+    private static final String TABLE_CATEGORY = "category";
+    private static final String COLUMN_CATEGORY_ID = "category_id";
+    private static final String COLUMN_CATEGORY_NAME = "category_name";
+
+    private static final String TABLE_PRODUCT = "product";
+    private static final String COLUMN_PRODUCT_ID = "product_id";
+    private static final String COLUMN_PRODUCT_NAME = "product_name";
+    private static final String COLUMN_PRODUCT_PRICE = "product_price";
+    private static final String COLUMN_PRODUCT_DESCRIPTION = "product_description";
+    private static final String COLUMN_PRODUCT_IMAGE = "product_image";
+    private static final String COLUMN_PRODUCT_CATEGORY = "product_category";
+
+
+
+
     public DBHelperUser(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
@@ -29,14 +44,34 @@ public class DBHelperUser extends SQLiteOpenHelper {
                 COLUMN_USER_NAME + " TEXT, " +
                 COLUMN_USER_EMAIL + " TEXT, " +
                 COLUMN_USER_PASSWORD + " TEXT, " +
-                COLUMN_USER_ROLE + " TEXT)"; // 1 = admin, 2 = user
+                COLUMN_USER_ROLE + " TEXT)";
+
+        String createTableCategory = "CREATE TABLE " + TABLE_CATEGORY + " (" +
+                COLUMN_CATEGORY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_CATEGORY_NAME + " TEXT)";
+
+        String createTableProduct = "CREATE TABLE " + TABLE_PRODUCT + " (" +
+                COLUMN_PRODUCT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_PRODUCT_NAME + " TEXT, " +
+                COLUMN_PRODUCT_PRICE + " TEXT, " +
+                COLUMN_PRODUCT_DESCRIPTION + " TEXT, " +
+                COLUMN_PRODUCT_IMAGE + " TEXT, " +
+                COLUMN_PRODUCT_CATEGORY + " TEXT)";
+
         db.execSQL(createTableUser);
+        db.execSQL(createTableCategory);
+        db.execSQL(createTableProduct);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String dropTableUser = "DROP TABLE IF EXISTS " + TABLE_USER;
+        String dropTableCategory = "DROP TABLE IF EXISTS " + TABLE_CATEGORY;
+        String dropTableProduct = "DROP TABLE IF EXISTS " + TABLE_PRODUCT;
         db.execSQL(dropTableUser);
+        db.execSQL(dropTableCategory);
+        db.execSQL(dropTableProduct);
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/mycake/ui/AdminDashboardActivity.java
+++ app/src/main/java/com/example/mycake/ui/AdminDashboardActivity.java
@@ -1,30 +1,95 @@
 package com.example.mycake.ui;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import com.example.mycake.HomeFragment;
+import com.example.mycake.ManageCakesFragment;
+import com.example.mycake.ManageCategoryFragment;
+import com.example.mycake.ManageOrdersFragment;
 import com.example.mycake.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class AdminDashboardActivity extends AppCompatActivity {
+public class AdminDashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    Toolbar toolbar;
+    DrawerLayout dl_admin_main;
+    NavigationView nv_admin_main;
+    Toolbar mtb_admin_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dl_admin_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        dl_admin_main = findViewById(R.id.dl_admin_main);
+        nv_admin_main = findViewById(R.id.nv_admin_main);
+
+        mtb_admin_main = findViewById(R.id.mtb_admin_main);
+        setSupportActionBar(mtb_admin_main);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dl_admin_main, mtb_admin_main, R.string.navigation_open, R.string.navigation_close);
+        dl_admin_main.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nv_admin_main.setNavigationItemSelectedListener(this);
+
+        // Load the default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_home,
+                    new HomeFragment()).commit();
+            nv_admin_main.setCheckedItem(R.id.nav_home);
+        }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        System.out.println("Item ID: " + itemId);
+
+       /* switch (item.getItemId()) {
+            case R.id.nav_home:
+                selectedFragment = new HomeFragment();
+                break;
+            case R.id.nav_manage_orders:
+                selectedFragment = new ManageOrdersFragment();
+                break;
+            case R.id.nav_manage_category:
+                selectedFragment = new ManageCategoryFragment();
+                break;
+            case R.id.nav_manage_cakes:
+                selectedFragment = new ManageCakesFragment();
+                break;
+            case R.id.nav_logout:
+                // Handle logout here
+                return true;
+        }*/
+
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fl_home,
+                    selectedFragment).commit();
+        }
+
+        dl_admin_main.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
